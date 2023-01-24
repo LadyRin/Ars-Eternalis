48bf0c9c3e26d524ea0130e4f68cc809aa5b0f00
Alex L <alexandre.letellier09@gmail.com>
2023-01-24 21:43:46 +0100

    AAA

--- Ars Eternalis/Assets/Scripts/Player/StateMachine/PlayerAirborneState.cs
+++ Ars Eternalis/Assets/Scripts/Player/StateMachine/PlayerAirborneState.cs
@@ -6,7 +6,7 @@ public class PlayerAirborneState : PlayerAliveState
 {
     public PlayerAirborneState(PlayerStateMachine currentContext) : base(currentContext) { }
 
-
+    LineRenderer lineRenderer;
     public override void CheckSwitchStates()
     {
         if (IsGrounded())
@@ -16,9 +16,45 @@ public class PlayerAirborneState : PlayerAliveState
         }
     }
 
+    protected override void Move()
+    {
+        Vector2 inputMove = context.InputMove;
+        float airAcceleration = context.AirAcceleration;
+        float maxAirSpeed = context.MaxAirSpeed;
+        Transform transform = context.transform;
+        Rigidbody rigidbody = context.Rigidbody;
+
+        Vector2 acceleration = inputMove * airAcceleration;
+        acceleration = transform.TransformDirection(new Vector3(acceleration.x, 0, acceleration.y));
+        Vector2 currentVelocity = new Vector2(rigidbody.velocity.x, rigidbody.velocity.z);
+
+        Vector2 i = currentVelocity.normalized == Vector2.zero ? Vector2.right : currentVelocity.normalized;
+        Vector2 j = Vector2.Perpendicular(i);
+
+        lineRenderer.SetPosition(0, transform.position);
+        lineRenderer.SetPosition(1, transform.position + new Vector3(i.x, 0, i.y) * 5);
+
+        float accelerationAlongI = Vector2.Dot(acceleration, i);
+        float accelerationAlongJ = Vector2.Dot(acceleration, j);
+
+        float velocityAlongI = Vector2.Dot(currentVelocity, i);
+        float velocityAlongJ = Vector2.Dot(currentVelocity, j);
+
+        if (accelerationAlongI > 0 && velocityAlongI > maxAirSpeed)
+            accelerationAlongI = 0;
+
+        if (accelerationAlongJ > 0 && velocityAlongJ > maxAirSpeed)
+            accelerationAlongJ = 0;
+
+        Vector2 accelerationInLocalSpace = accelerationAlongI * i + accelerationAlongJ * j;
+        Vector3 accelerationInWorldSpace = transform.TransformDirection(new Vector3(accelerationInLocalSpace.x, 0, accelerationInLocalSpace.y));
+
+        rigidbody.AddForce(accelerationInWorldSpace, ForceMode.Acceleration);
+    }
+
     public override void EnterState()
     {
-        //
+        lineRenderer = context.GetComponent<LineRenderer>();
     }
 
     public override void ExitState()
---
+++ Ars Eternalis/Assets/Scripts/Weapons/Firethrower.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Firethrower : Weapon
+{
+    protected override bool CanUse()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void Use()
+    {
+        throw new System.NotImplementedException();
+    }
+}
