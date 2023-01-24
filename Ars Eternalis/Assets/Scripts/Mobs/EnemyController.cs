3bc338b0d938130d71ccdc13cadc0bd69b69ed0c
Theodore <theodorebjpicot@gmail.com>
2023-01-24 22:34:37 +0100

    EnemyAIv3

--- Ars Eternalis/Assets/Scripts/Mobs/EnemyController.cs
+++ Ars Eternalis/Assets/Scripts/Mobs/EnemyController.cs
@@ -6,20 +6,16 @@ public class EnemyController : MonoBehaviour
     Transform target;
     NavMeshAgent agent;
 
-    // Patrolling 
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange; 
     // Attacking
     public float timeBetweenAttacks;
     bool hasAlreadyAttacked;
+    public GameObject projectile;
+    public float health;
 
     //States
     public float attackRange;
     public bool playerInSightRange;
     public bool playerInAttackRange = true; 
-
-    private  
 
     // Start is called before the first frame update
     void Start()
@@ -31,23 +27,40 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         float distance = Vector3.Distance(target.position, transform.position);
         playerInAttackRange = distance <= attackRange;
         if (!playerInAttackRange) ChasePlayer();
         if (playerInAttackRange) AttackPlayer();
     }
-    
-    private void SearchWalkPoint() {
-        // float randomZ = Random.Range()
-    }
 
     private void ChasePlayer() {
         agent.SetDestination(target.position);
+        transform.LookAt(target);
+        if (!hasAlreadyAttacked) {
+            Rigidbody rb = Instantiate(projectile, transform.position, Quaternion.identity).GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+            rb.AddForce(transform.up * 32f, ForceMode.Impulse);
+
+            hasAlreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
     }
 
     private void AttackPlayer() {
+        // agent.Stop()
+    }
 
+    private void ResetAttack() {
+        hasAlreadyAttacked = false;
+    }
+
+    public void TakeDamage(int damage) {
+        health -= damage;
+        if (health <= 0) DestroyEnemy();
+    }
+
+    private void DestroyEnemy() {
+        
     }
 
     void OnDrawGizmosSelected() {
