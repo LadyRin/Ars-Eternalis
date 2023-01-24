12eca18605f0c0b675a64a4f0636dc063f350978
Loïs <lois.odiardo@gmail.com>
2023-01-24 18:47:09 +0100

    zonzon qui avance

--- Ars Eternalis/Assets/Scripts/Mobs/ZombieController.cs
+++ Ars Eternalis/Assets/Scripts/Mobs/ZombieController.cs
@@ -6,16 +6,29 @@ public class ZombieController : MonoBehaviour
 {
 
     Animator animator;
+    GameObject player;
+    float moveSpeed =1.0f;
+    float detectionRange =5.0f;
+    float attackRange=0.5f;
     // Start is called before the first frame update
     void Start()
     {
         animator=GetComponent<Animator>();
-        
+        animator.SetBool("isWalking",true);
+        player = GameObject.Find("player");
     }
 
     // Update is called once per frame
     void Update()
     {
+        float distanceToPlayer = Vector3.Distance(transform.position,player.transform.position);
+
+        if(distanceToPlayer<=detectionRange){
+            animator.SetBool("isWalking",true);
+        }else{
+            animator.SetBool("isWalking",false);
+        }
         
     }
+
 }
