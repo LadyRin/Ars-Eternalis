f231fa67e99bc210fa9510130680cdb25cb90324
Alex L <alexandre.letellier09@gmail.com>
2023-01-25 05:12:29 +0100

    commmlt

--- Ars Eternalis/Assets/Scripts/ProceduralGenerator.cs
+++ Ars Eternalis/Assets/Scripts/ProceduralGenerator.cs
@@ -6,34 +6,15 @@ using Random = UnityEngine.Random;
 
 public class ProceduralGenerator : MonoBehaviour
 {
-    [SerializeField] private GameObject[] prefabs;
-    private bool[][][] map = new bool[100][][];
-
+    [SerializeField] GameObject endRoomPrefab;
     void Start()
     {
-        for (int i = 0; i < map.Length; i++)
-        {
-            bool[][] zMap = new bool[100][];
-            for (int j = 0; j < zMap.Length; j++)
-            {
-                zMap[j] = new bool[2];
-            }
-            map[i] = zMap;
-        }
-
-        AddCenter();
+        AddEndRoom();
     }
 
-    void AddCenter()
-    {
-        map[50][50][0] = true;
-        GameObject center = Instantiate(prefabs[Random.Range(1, 12)], new Vector3(0, 0, 0), Quaternion.identity);
-        var sorties = center.GetComponentsInChildren<Sortie>();
-        Debug.Log(sorties.Length);
-        foreach (var sortie in sorties)
-        {
-            sortie.CreateRoom(prefabs[13]); //Random.Range(0, prefabs.Length)
-        }
+    void AddEndRoom() {
+        GameObject go = Instantiate(endRoomPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        go.GetComponent<Room>().Expand(3);
     }
 
 
--- Ars Eternalis/Assets/Scripts/Sortie.cs
+++ Ars Eternalis/Assets/Scripts/Sortie.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class Sortie : MonoBehaviour
 {
+    static GameObject[] prefabs1x1;
+    static GameObject[] prefabs1x2;
+    static GameObject wallPrefab;
+
     public void CreateRoom(GameObject prefab) {
-        Debug.Log("Function called");
         GameObject room = Instantiate(prefab, transform.position, transform.rotation);
-        //room.transform.parent = 
+        
+    }
+
+    public void CreateWall() {
+        Instantiate(wallPrefab, transform.position, transform.rotation);
+    }
+
+    public Room CreateRoom1x1() {
+        GameObject go = Instantiate(prefabs1x1[Random.Range(0, prefabs1x1.Length)], transform.position, transform.rotation);
+        return go.GetComponent<Room>();
+    }
+
+    public Room CreateRoom1x2() {
+        GameObject go = Instantiate(prefabs1x2[Random.Range(0, prefabs1x2.Length)], transform.position, transform.rotation);
+        return go.GetComponent<Room>();
+    }
+
+
+    //Returns 0 if no space, 1 if there is a 30x30 space in front, 2 if there is a 30x60 space in front
+    //Use physics to check for space
+    public int CheckSpace() {
+        if(Physics.Raycast(transform.position, transform.forward, 30)) {
+            if(Physics.Raycast(transform.position, transform.forward, 60)) {
+                return 2;
+            }
+            return 1;
+        }
+        return 0;
     }
 
 }
